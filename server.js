0ae053ef4c985868c4d9b873b51ef398ad2bd95b
belaidaymen444 <belaidaymen444@gmail.com>
2024-09-30 21:52:22 +0100

    feat(add ejs and under work tailwind and html and server.js)

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
-
+import { writeFile } from "fs/promises";
 const app = express()
-
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 app.get("/",  (rsq,res)=>{
     
-    res.get("/index.html") 
+    res.status(200).send("Hello")
+})
+app.get("/info",(req,res)=>{
+    writeFile("users.csv",JSON.stringify(req.body))
+    res.status(200).render("view/admin.ejs", req.body);
 })
 app.use(express.static("public"))
 
